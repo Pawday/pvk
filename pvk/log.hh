259ea959736a607770ef854edf5073b289268a36
Pavel Korotkevich <pawday@mail.ru>
2024-02-18 12:25:05 +0300

    Trace macro

--- pvk/log.hh
+++ pvk/log.hh
@@ -7,6 +7,10 @@ namespace log {
 void error(const std::string &message) noexcept;
 void warning(const std::string &message) noexcept;
 void info(const std::string &message) noexcept;
+
 void trace(const std::string &message, const std::string &source = "") noexcept;
+
+// You need to include std::format to use this macro
+#define PVK_TRACE(message) log::trace(message, std::format("{}:{}", __FILE_NAME__, __LINE__))
 
 } // namespace log
